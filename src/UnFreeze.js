66c14d0053c35b814502e806c8c56f1a20e21077
Marek Jędryka <marek629@poczta.onet.pl>
2017-10-29 17:51:11 +0100

    Added callback iterations count support.

--- src/UnFreeze.js
+++ src/UnFreeze.js
@@ -2,10 +2,15 @@ class UnFreeze {
     /**
      * @public
      * @param {Array} container
+     * @param {Function} callback Event loop callback.
+     * @param {boolean} doneOnCallback
      */
-    constructor(container) {
+    constructor(container, callback, doneOnCallback = false, intervalIterations = 1) {
         this.index = 0;
         this.data = container;
+        this.eventLoop = callback;
+        this.doneOnEventLoop = doneOnCallback;
+        this.intervalIterations = intervalIterations;
     }
 
     /**
@@ -16,13 +21,26 @@ class UnFreeze {
         return {
             next: () => {
                 if (this.index < this.data.length) {
-                    return {value: this.data[this.index++], done: false};
+                    let done = false;
+                    if (this.index > 0 && this.index % this.intervalIterations === 0) {
+                        this.eventLoop(this);
+                        done = this.doneOnEventLoop;
+                    }
+                    return {value: this.data[this.index++], done: done};
                 } else {
                     this.index = 0; //If we would like to iterate over this again without forcing manual update of the index
                     return {done: true};
                 }
             }
         }
+    }
+
+    /**
+     * @public
+     * @returns {Array.<T>}
+     */
+    get elementsLeft() {
+        return this.data.slice(this.index);
     }
 }
 
--- tests/cli/array.js
+++ tests/cli/array.js
@@ -3,10 +3,16 @@ import test from "ava";
 import UnFreeze from "../../src/UnFreeze";
 
 
-test("iterate array by for...of loop", t => {
-    t.plan(3);
+test.serial("iterate array by for...of loop", t => {
+    const n = 30;
 
-    let uf = new UnFreeze([1, 2, 3]);
+    t.plan(n);
+
+    const array = Array.from(new Array(n).keys());
+    const uf = new UnFreeze(array, u => {
+        console.log('event loop');
+        console.log(`UnFreeze object: ${u.elementsLeft}`)
+    });
 
     for (const element of uf) {
         console.log('array element: ' + element);
@@ -14,4 +20,26 @@ test("iterate array by for...of loop", t => {
     }
 });
 
+test.serial("iterate array by for...of loop driven by callback", t => {
+    const n = 30;
 
+    t.plan(n);
+
+    const array = Array.from(new Array(n).keys());
+
+    const callback = u => {
+        console.log('event loop');
+        const uf = new UnFreeze(u.elementsLeft, callback, true, 10);
+        for (const element of uf) {
+            console.log('array element: ' + element);
+            console.log(`UnFreeze object: ${u.elementsLeft}`);
+            t.pass();
+        }
+    };
+
+    const uf = new UnFreeze(array, callback, true, 10);
+    for (const element of uf) {
+        console.log('array element: ' + element);
+        t.pass();
+    }
+});
